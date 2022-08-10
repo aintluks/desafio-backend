9f0860ea9ee628a45c0235ecb25c1fb09ca67e74
Lucas da Costa <aintluks@gmail.com>
2022-08-10 19:00:08 -0300

    Refact ExpensesController

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,9 +1,9 @@
 class ExpensesController < ApplicationController
   def show
     @file_upload = FileUpload.new
-    @expenses ||= Expense.find_by_deputy_id(params[:id])
-    @greater_expense ||= @expenses.greater_expense(@expenses)
+    @expenses = Expense.find_by_deputy_id(params[:id])
+    @greater_expense = @expenses.greater_expense(@expenses)
+    @total = @expenses.expenses_total(@expenses)
     @data = @expenses.chart_data(@expenses)
-    @total ||= @expenses.expenses_total(@expenses)
   end
 end
