86e5191f7e5a328e20c73a1a7292bf765e27143b
Lucas da Costa <aintluks@gmail.com>
2022-08-10 15:40:45 -0300

    Refact on show expenses and add chart

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,6 +1,9 @@
 class ExpensesController < ApplicationController
   def show
     @file_upload = FileUpload.new
-    @expenses = Expense.where(deputy_id: params[:id])
+    @expenses ||= Expense.find_by_deputy_id(params[:id])
+    @greater_expense ||= @expenses.greater_expense(@expenses)
+    @data = @expenses.chart_data(@expenses)
+    @total ||= @expenses.expenses_total(@expenses)
   end
 end
