daf24e4802cbc5014568754d856b40ede5afc17e
Lucas da Costa <aintluks@gmail.com>
2022-08-10 19:13:16 -0300

    Refact variable name

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,7 +2,7 @@ class ExpensesController < ApplicationController
   def show
     @file_upload = FileUpload.new
     @expenses = Expense.find_by_deputy_id(params[:id])
-    @greater_expense = @expenses.greater_expense(@expenses)
+    @higher_spending = @expenses.higher_spending(@expenses)
     @total = @expenses.expenses_total(@expenses)
     @data = @expenses.chart_data(@expenses)
   end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,7 +3,7 @@ class Expense < ApplicationRecord
 
   # Scopes
   scope :find_by_deputy_id, -> (deputy_id) { where(deputy_id: deputy_id) }
-  scope :greater_expense, -> (expenses) { expenses.maximum(:vlr_liquido) }
+  scope :higher_spending, -> (expenses) { expenses.maximum(:vlr_liquido) }
   scope :expenses_total, -> (expenses) { expenses.sum(:vlr_liquido) }
   scope :chart_data, -> (expenses) { expenses.group_by_month(:dat_emissao, format: "%b %Y").sum(:vlr_liquido) }
 end
