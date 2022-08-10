f1f8192c67c090e095c5a5715063ed0c8569f5f0
Lucas da Costa <aintluks@gmail.com>
2022-08-10 16:53:07 -0300

    Test to FileUploadsController

---
+++ spec/controllers/file_uploads_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe FileUploadsController do
+  describe 'POST#create' do
+    let(:file) { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'dados-abertos.csv'), 'text/csv') }
+
+    let(:params) do
+      {
+        file_upload: {
+          csv_file: file
+        }
+      }
+    end
+
+    it 'creates a new FileUpload' do
+      expect(CsvImporter).to receive(:perform).with(csv: anything)
+
+      expect { post :create, params: params }.to change(FileUpload, :count).by(1)
+    end
+
+    it 'does not create duplicated FileUpload' do
+      expect { post :create, params: params }.to change(FileUpload, :count).by(1)
+      expect { post :create, params: params }.not_to change(FileUpload, :count)
+    end
+  end
+end
